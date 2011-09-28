f50cb647adee32920f1fa01fc6e80ea03f07d48f
Drew Walton <drewwalton19216801@gmail.com>
2011-09-28 09:52:12 -0600

    Revert "cpufreq: Prevent memory leak in cpufreq_stats on hotplug" Whoopsies

--- drivers/cpufreq/cpufreq_stats.c
+++ drivers/cpufreq/cpufreq_stats.c
@@ -338,13 +338,9 @@ static int __cpuinit cpufreq_stat_cpu_callback(struct notifier_block *nfb,
 	case CPU_ONLINE_FROZEN:
 		cpufreq_update_policy(cpu);
 		break;
-	case CPU_DOWN_PREPARE:
-	case CPU_DOWN_PREPARE_POWER:
+	case CPU_DEAD:
+	case CPU_DEAD_FROZEN:
 		cpufreq_stats_free_table(cpu);
-		break;
-	case CPU_DOWN_FAILED:
-	case CPU_DOWN_FAILED_FROZEN:
-		cpufreq_stats_create_table_cpu(cpu);
 		break;
 	}
 	return NOTIFY_OK;
@@ -353,7 +349,6 @@ static int __cpuinit cpufreq_stat_cpu_callback(struct notifier_block *nfb,
 static struct notifier_block cpufreq_stat_cpu_notifier __refdata =
 {
 	.notifier_call = cpufreq_stat_cpu_callback,
-	.priority 1,
 };
 
 static struct notifier_block notifier_policy_block = {
--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 4 */
+/* This file is auto generated, version 5 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#4 PREEMPT Wed Sep 28 09:01:14 MDT 2011"
-#define LINUX_COMPILE_TIME "09:01:14"
+#define UTS_VERSION "#5 PREEMPT Wed Sep 28 09:31:28 MDT 2011"
+#define LINUX_COMPILE_TIME "09:31:28"
 #define LINUX_COMPILE_BY "lg-electronics"
 #define LINUX_COMPILE_HOST "drewwalton-MS-7549"
 #define LINUX_COMPILE_DOMAIN "(none)"
