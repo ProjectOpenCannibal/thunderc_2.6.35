9ded4b5e666c1d8e20159ebeba9ec116b6988f73
Drew Walton <drewwalton19216801@gmail.com>
2011-09-29 10:03:43 -0600

    Whoops!

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,9 +1,9 @@
 /* This file is auto generated, version 6 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#6 PREEMPT Wed Sep 28 10:45:53 MDT 2011"
-#define LINUX_COMPILE_TIME "10:45:53"
-#define LINUX_COMPILE_BY "lg-electronics"
+#define UTS_VERSION "#6 PREEMPT Thu Sep 29 10:02:43 MDT 2011"
+#define LINUX_COMPILE_TIME ""
+#define LINUX_COMPILE_BY "drewwalton"
 #define LINUX_COMPILE_HOST "drewwalton-MS-7549"
 #define LINUX_COMPILE_DOMAIN "(none)"
 #define LINUX_COMPILER "gcc version 4.5.2 (Ubuntu/Linaro 4.5.2-8ubuntu3) "
