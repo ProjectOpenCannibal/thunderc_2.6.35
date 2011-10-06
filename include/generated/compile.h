74dc49b8d26ca90e4a4346122431ed32c44719ec
Drew Walton <drewwalton19216801@gmail.com>
2011-10-06 08:35:11 -0600

    Version bump to v06

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,9 +1,9 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 1 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 PREEMPT Mon Oct 3 11:30:45 MDT 2011"
-#define LINUX_COMPILE_TIME "11:30:45"
+#define UTS_VERSION "#1 PREEMPT Thu Oct 6 02:20:19 MDT 2011"
+#define LINUX_COMPILE_TIME "02:20:19"
 #define LINUX_COMPILE_BY "drewwalton"
-#define LINUX_COMPILE_HOST "drewwalton-MS-7549"
+#define LINUX_COMPILE_HOST "buildhost"
 #define LINUX_COMPILE_DOMAIN "(none)"
 #define LINUX_COMPILER "gcc version 4.5.2 (Ubuntu/Linaro 4.5.2-8ubuntu3) "
