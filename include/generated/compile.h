db2862949abdd80621dddead6a8a3cb0b284b0fe
Drew Walton <drewwalton19216801@gmail.com>
2011-10-07 10:55:34 -0600

    Version bump to v06.2pre

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
 /* This file is auto generated, version 1 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 PREEMPT Fri Oct 7 10:00:01 MDT 2011"
-#define LINUX_COMPILE_TIME "10:00:01"
+#define UTS_VERSION "#1 PREEMPT Fri Oct 7 10:52:28 MDT 2011"
+#define LINUX_COMPILE_TIME "10:52:28"
 #define LINUX_COMPILE_BY "drewwalton"
 #define LINUX_COMPILE_HOST "drewwalton-MS-7549"
 #define LINUX_COMPILE_DOMAIN "(none)"
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "2.6.35.7-GingerKernel-v06.1+"
+#define UTS_RELEASE "2.6.35.7-GingerKernel-v06.2pre+"
