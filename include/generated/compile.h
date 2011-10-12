4397f15731bfa8a5c30bb44040c7c5ec9990214f
Drew Walton <drewwalton19216801@gmail.com>
2011-10-12 09:47:42 -0600

    Random bullshit

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 2 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 PREEMPT Mon Oct 10 10:54:19 MDT 2011"
-#define LINUX_COMPILE_TIME "10:54:19"
+#define UTS_VERSION "#2 PREEMPT Mon Oct 10 15:54:26 MDT 2011"
+#define LINUX_COMPILE_TIME "15:54:26"
 #define LINUX_COMPILE_BY "drewwalton"
 #define LINUX_COMPILE_HOST "drewwalton-MS-7549"
 #define LINUX_COMPILE_DOMAIN "(none)"
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "2.6.35.7-GingerKernel-v07pre3+"
+#define UTS_RELEASE "2.6.35.7-GingerKernel-v08pre+"
