b8183b95e19732131e21e1751290cf6a58cd93d1
Drew Walton <drewwalton19216801@gmail.com>
2011-10-29 16:57:45 -0600

    VIDEO: framebuffer misplaced screen fix (?)

--- drivers/video/msm/mdp.c
+++ drivers/video/msm/mdp.c
@@ -43,6 +43,10 @@
 #include "mdp4.h"
 #endif
 
+#ifdef CONFIG_HAS_EARLYSUSPEND
+#undef CONFIG_HAS_EARLYSUSPEND
+#endif
+
 uint32 mdp4_extn_disp;
 static struct clk *mdp_clk;
 static struct clk *mdp_pclk;
