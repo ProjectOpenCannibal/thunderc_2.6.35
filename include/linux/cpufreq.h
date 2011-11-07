c56403d820531153c6cb8cbd83bdaca8d9b1a95d
Drew Walton <drewwalton19216801@gmail.com>
2011-11-06 23:49:33 -0700

    cpufreq: msm7k governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -375,6 +375,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
 extern struct cpufreq_governor cpufreq_gov_smartass2;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_smartass2)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_MSM7K)
+extern struct cpufreq_governor cpufreq_gov_msm7k;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_msm7k)
 #endif
 
 
